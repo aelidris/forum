9faea18de526abf88a00d3021a0d54ea25a2abb8
aelidris <smias1.1998@gmail.com>
2024-12-21 12:20:02 +0100

    Some modification in styles

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module forum
 go 1.22.3
 
 require (
-	github.com/gofrs/uuid v4.4.0+incompatible // indirect
-	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/mattn/go-sqlite3 v1.14.24
 )
--- main.go
+++ main.go
@@ -2,32 +2,23 @@ package main
 
 import (
 	"fmt"
-	"forum/database"
 	"net/http"
 
+	"forum/database"
+
 	"forum/handlers"
-	
 )
 
-func main(){
+func main() {
+	database.InitDB()
 
-
-database.InitDB()
-
-
-    http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/register", handlers.RegisterHandler)
 	http.HandleFunc("/login", handlers.LoginHandler)
-	 http.HandleFunc("/logout", handlers.LogoutHandler)
+	http.HandleFunc("/logout", handlers.LogoutHandler)
 	http.HandleFunc("/make-post", handlers.CreatePostHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
-
-
-
 	fmt.Println("server starting at: http://localhost:8080/")
-    http.ListenAndServe(":8080", nil)
-
-
-
+	http.ListenAndServe(":8080", nil)
 }
