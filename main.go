4641d8e398dfe40b1ab5fff2965f59dd0e3c0f01
aelidris <smias1.1998@gmail.com>
2024-12-31 13:48:49 +0100

    struct of main func

--- main.go
+++ main.go
@@ -12,14 +12,12 @@ import (
 
 func main() {
 	database.InitDB()
-
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/register", handlers.RegisterHandler)
 	http.HandleFunc("/login", handlers.LoginHandler)
 	http.HandleFunc("/logout", handlers.LogoutHandler)
 	http.HandleFunc("/make-post", handlers.CreatePostHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-
 	fmt.Println("server starting at: http://localhost:8080/")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
